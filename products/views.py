7fa4b3f8b09c95fc667d884f1d2838bbe8ed2ebe
Seun Ajetomobi <fullstackfory@gmail.com>
2022-07-07 15:05:35 +0000

    Added add to bag functionality

--- products/views.py
+++ products/views.py
@@ -5,8 +5,8 @@ from django.db.models.functions import Lower
 
 from .models import Product, Category
 
-# Create your views here.
 
+# Create your views here.
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
@@ -30,7 +30,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -41,7 +41,7 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
